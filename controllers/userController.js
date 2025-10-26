7117e2c6768319f68f75e338e825a30b41649981
Athirakrishn <athiraramakrishnan41020@gmail.com>
2025-10-26 23:10:45 +0530

    user register

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,10 +2,8 @@
 
 exports.registerLogicAPI = (req, res) => {
   console.log("inside register api")
-  // console.log(req.body)
- const {username,email,password} =req.body
- console.log(username,email,password);
- 
+  const { username, email, password } = req.body
+  console.log(username, email, password)
   res.status(200).send("register request received")
 }
 
--- routing/router.js
+++ routing/router.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const userController = require('../controllers/userController')
 const router = express.Router()
- 
+
 // register route
-router.post('/register',userController.registerController )
+router.post('/register', userController.registerLogicAPI)
 
 module.exports = router
