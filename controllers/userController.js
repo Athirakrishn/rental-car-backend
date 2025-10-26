a4d2d9cc06cdc7839329cb66e5e6e7920912ff23
Athirakrishn <athiraramakrishnan41020@gmail.com>
2025-10-26 22:41:01 +0530

    user  register

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+//register controller
+
+exports.registerLogicAPI = (req, res) => {
+  console.log("inside register api")
+  // console.log(req.body)
+ const {username,email,password} =req.body
+ console.log(username,email,password);
+ 
+  res.status(200).send("register request received")
+}
+
+//login controller
+
+
+//profile pic
---
+++ index.js
@@ -0,0 +1,29 @@
+//import dotenv cors express
+//load .env file contents into process.env
+require('dotenv').config()
+const express =require('express')
+const cors=require('cors')
+const router = require('./routing/router')
+
+//create server
+const rentalCarServer = express()
+
+//enable cors protocol in server app
+rentalCarServer.use(cors())
+rentalCarServer.use(express.json())
+rentalCarServer.use(router)
+//create port 
+const PORT = 3000
+//run server port 
+rentalCarServer.listen(PORT,()=>{
+  console.log(`rental car started port:${PORT}`);
+ 
+})
+
+//resolving http request
+// rentalCarServer.get('/',(req,res)=>{
+//   res.status(200).send('rental car started ')
+// })
+rentalCarServer.post('/',(req,res)=>{
+  res.status(200).send('POST REQUEST')
+})
