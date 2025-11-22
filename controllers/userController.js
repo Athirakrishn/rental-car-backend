fac4cf513ef0aa715a22c6d0f3061ea13d30bad8
Athirakrishn <athiraramakrishnan41020@gmail.com>
2025-11-22 23:58:17 +0530

    google login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,95 @@
-//register controller
+const users = require('../models/userModel')
+const jwt =require('jsonwebtoken')
+//register
+exports.registerController= async (req,res)=>{
+    console.log("inside register API");
+    
+    // console.log(req.body);
+    const{username,email,password} =req.body
+        console.log(username,email,password);
+        try{
+            const existingUser= await users.findOne({email})
+            if(existingUser){
+                res.status(409).json("user already exist please login ")
+            }else{
+                const newUser= new users({
+                    username,
+                    email,
+                    password
+                })
+                await newUser.save()
+                res.status(200).json(newUser)
+            }
+        }catch(err){ 
+           res.status(500).json(err)
+        }
 
-exports.registerLogicAPI = (req, res) => {
-  console.log("inside register api")
-  const { username, email, password } = req.body
-  console.log(username, email, password)
-  res.status(200).send("register request received")
+  
+ }
+
+//login
+exports.loginController = async (req,res)=>{
+    console.log("Inside Login API");
+
+    // console.log(req.body);
+    const {email,password} = req.body
+    console.log(email,password);
+    
+    try{
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+           if(existingUser.password== password)
+           {//token
+          const token = jwt.sign({userMail:existingUser.email,role:existingUser.role},process.env.JWTSECRET)
+          res.status(200).json({user:existingUser,token})
+        //   console.log("JWT Secret:", process.env.JWTSECRET);
+           }
+           else{ 
+            res.status(401).json("Invalid Email and Password")
+           } 
+        }else{
+           res.status(404).json("account does not exist...")
+
+        }
+    }catch(err){
+        res.status(500).json(err)
+    }
+    
 }
 
-//login controller
+
+//googlelogin
+exports.googleLoginController = async (req, res) => {
+    console.log("Inside Google Login API");
+    // console.log(req.body);
+    const { email, password, username, profile } = req.body
+    console.log(email, password, username, profile);
+
+    try {
+        const existingUser = await users.findOne({ email })
+        if (existingUser) {
+            //token
+            const token = jwt.sign({ userMail: existingUser.email,role:existingUser.role }, process.env.JWTSECRET)
+            res.status(200).json({ user: existingUser, token })
+        } else {
+            const newUser = new users({
+                username, email, password, profile
+            })
+            await newUser.save()
+            //token
+            const token = jwt.sign({ userMail: newUser.email }, process.env.JWTSECRET)
+            res.status(200).json({ user: newUser, token })
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
+}
 
 
-//profile pic
+
+
+
+
+
+
--- routing/router.js
+++ routing/router.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const userController = require('../controllers/userController')
+const jwtMiddleware = require('../middlewares/jwtMiddleware')
 const router = express.Router()
 
+//register
+router.post('/register', userController.registerController)
 
-router.post('/register', userController.registerLogicAPI)
+//login
+router.post('/login',userController.loginController)
+
+
+//Google-login
+router.post('/google-login', userController.googleLoginController)
 
 module.exports = router
